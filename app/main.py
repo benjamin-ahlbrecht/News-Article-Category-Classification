559ca377b79b52101c4c67da59544b096c66a242
Benjamin Ahlbrecht <benjaminahlbrecht@gmail.com>
2022-11-05 14:10:49 -0500

    Add FastAPI base back-end | Connect API to project at root

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.encoders import jsonable_encoder
+
+from jinja2.exceptions import TemplateNotFound
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/scripts", StaticFiles(directory="scripts"), name="scripts")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_project(request:Request):
+    return templates.TemplateResponse(
+        name="projects/project.html",
+        context={"request": request}
+    )
